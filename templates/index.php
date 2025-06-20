821e94113ab5179574237dfb868776ffa9c4f93e
João Paulo <joaopauloresendecosta55@gmail.com>
2025-06-20 19:51:35 -0300

    Refactor index page: update contact table structure and remove placeholder text

--- templates/index.php
+++ templates/index.php
@@ -9,7 +9,19 @@
             </div>
             <h1 id="main-title">Meus Clientes</h1>
             <?php if(count($contats) > 0): ?>
-                <p>TEM CONTATOS</p>
+                <table class="table" id="contacts-table">
+                    <thead>
+                        <tr>
+                           <th scope="col">#</th>
+                           <th scope="col">Nome</th> 
+                           <th scope="col">Telefone</th>
+                           <th scope="col">Valor a ser Pago</th>
+                           <th scope="col">Produtos</th>  
+                           <th scope="col">#</th> 
+                        </tr>
+                    </thead>
+                </table>
+                
             <?php else: ?>
                 <p id="empty-list-text">Ainda não Há clientes, <a href="<?= $BASE_URL ?>create.php" >Clique aqui para adicionar</a></p>
             <?php endif; ?>
