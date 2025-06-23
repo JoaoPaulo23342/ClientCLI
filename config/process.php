ccc30ec25a67bd9fb042fa6cc4390a779a86c83d
João Paulo <joaopauloresendecosta55@gmail.com>
2025-06-23 10:47:25 -0300

    Refactor process logic and adjust CSS: implement contact creation logic and modify line height for better readability

--- config/process.php
+++ config/process.php
@@ -4,24 +4,62 @@
  include_once("connection.php");
  include_once("url.php");
 
-$id;
-if(!empty($_GET)) {
-    $id = $_GET['id'];
+ $data = $_POST;
+ if(!empty($data)) {
+
+     
+
+     if($data['type'] === 'create') {
+        $name = $data['name'];
+        $amount = $data['amountDue'];
+        $phone = $data['phone'];
+        $cpf = $data['CPF'];
+        $adress = $data['Adress'];
+        $products = $data['products'];
+        $observation = $data['observation'];
+
+        $query = "INSERT INTO contacts (name, phone, CPF, adress, products, observation, AmountDue) VALUES (:name, :phone, :CPF, :Adress, :products, :observation, :amountDue)";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':phone', $phone);
+        $stmt->bindParam(':CPF', $cpf);
+        $stmt->bindParam(':Adress', $adress);
+        $stmt->bindParam(':products', $products);
+        $stmt->bindParam(':observation', $observation);
+        $stmt->bindParam(':amountDue', $amount);
+        try {
+            $stmt->execute();
+            $_SESSION['msg'] = "Contato adicionado com sucesso!";
+            
+    }   catch (PDOException $e) {
+        $error = $e->getMessage();
+        echo "Erro: " . $error;
+        exit; // Interrompe a execução se houver erro na conexão
+}
+        
+     }
+ } else {
+    $id;
+    if(!empty($_GET)) {
+      $id = $_GET['id'];
 }
 //retorna o dado de um contato
-if(!empty($id)) {
-    $query = "SELECT * FROM contacts WHERE id = :id";
-    $stmt = $conn->prepare($query);
-    $stmt->bindParam(':id', $id);
-    $stmt->execute();
-    $contact = $stmt->fetch();
-} else {
+    if(!empty($id)) {
+     $query = "SELECT * FROM contacts WHERE id = :id";
+     $stmt = $conn->prepare($query);
+     $stmt->bindParam(':id', $id);
+     $stmt->execute();
+     $contact = $stmt->fetch();
+}   else {
     // retorna todos os contatos do banco de dados
-    $contats = [];
-    $query = "SELECT * FROM contacts";
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
-    $contats = $stmt->fetchAll();
+     $contats = [];
+     $query = "SELECT * FROM contacts";
+     $stmt = $conn->prepare($query);
+     $stmt->execute();
+     $contats = $stmt->fetchAll();
 }
+ }
+
+
 
 
